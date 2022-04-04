c68ac05a1269b5f2d5cab565b3d0ec1b02855180
dudarsantos <eduardarsantos99@gmail.com>
2022-04-04 11:34:10 -0300

    inatalação de cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import "reflect-metadata";
 import express from 'express';
+import cors from 'cors';
 import "./database";
 import { routes } from "./routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
