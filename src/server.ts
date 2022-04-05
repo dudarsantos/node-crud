ff59ed71975c06b3a55b01b5562b941d59e71530
dudarsantos <eduardarsantos99@gmail.com>
2022-04-05 17:24:53 -0300

    ajuste no limite de tamanho para receber base64

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,9 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
+
+app.use(express.urlencoded({extended: true, limit: "10mb"}));
 
 app.use(routes)
 
